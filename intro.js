ddc73e17567ed8ae324797ce1ab17c8387ac4775
EduardoViveros <A01710605@tec.mx>
2026-02-12 12:38:39 -0600

    Añadi archivo de js

---
+++ intro.js
@@ -0,0 +1,9 @@
+console.log("Welcome to the Software Development Workshop!");
+console.info("Del Univeros de Marvel Comics")
+console.warn("Esto no es Dcomics");
+console.error("Error: No busques a Batman aqui");
+
+// el operador == es para comparacion de valores
+console.assert(1 == true, "Esto es una asercion");
+// el operador == es para comparacion de valores
+console.assert(1 === true, "Esto es una asercion, y no se cumple");

c0479fccb5d04890f11a07966ea758fe783e49d2
EduardoViveros <A01710605@tec.mx>
2026-02-15 23:40:07 -0600

    agrugue caso de estudio All about Pools

--- intro.js
+++ intro.js
@@ -7,3 +7,13 @@ console.error("Error: No busques a Batman aqui");
 console.assert(1 == true, "Esto es una asercion");
 // el operador == es para comparacion de valores
 console.assert(1 === true, "Esto es una asercion, y no se cumple");
+
+
+// ----- Variables y constantes -----
+// Forma tradicional de declarar variables
+// Tiene mayor alcance por lo que no es recomendable usarla
+var ScarletWitch = "WandaMaximoff";
+
+// Forma moderna de declarar variables
+// La variable solo vive dentro de las llaves donde se declara
+let Vision = "Vision";

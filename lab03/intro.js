f6ce605981747ef8972e8b8d943e2f95c132bddb
EduardoViveros <A01710605@tec.mx>
2026-02-15 23:59:37 -0600

    reacomode labs 01 y 03

---
+++ lab03/intro.js
@@ -0,0 +1,19 @@
+console.log("Welcome to the Software Development Workshop!");
+console.info("Del Univeros de Marvel Comics")
+console.warn("Esto no es Dcomics");
+console.error("Error: No busques a Batman aqui");
+
+// el operador == es para comparacion de valores
+console.assert(1 == true, "Esto es una asercion");
+// el operador == es para comparacion de valores
+console.assert(1 === true, "Esto es una asercion, y no se cumple");
+
+
+// ----- Variables y constantes -----
+// Forma tradicional de declarar variables
+// Tiene mayor alcance por lo que no es recomendable usarla
+var ScarletWitch = "WandaMaximoff";
+
+// Forma moderna de declarar variables
+// La variable solo vive dentro de las llaves donde se declara
+let Vision = "Vision";
